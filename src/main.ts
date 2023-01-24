db3c81b17c1976ed6a8053d24bf1399d6dac4e68
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-01-24 10:10:46 -0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
 
-if(!process.env.IS_TS_NODE){
-  require('module-alias/register')
-}
+// if(!process.env.IS_TS_NODE){
+//   require('module-alias/register')
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  console.log(process.env.PORT)
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
