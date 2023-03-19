68975626ebcbee2843a5e8a1f2abb52a381dc731
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-03-19 17:05:37 -0100

    Update ormconfig.ts

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -5,6 +5,7 @@ const config: PostgresConnectionOptions = {
   type: 'postgres',
   host: process.env.PGHOST || 'localhost',
   port: parseInt(process.env.PGPORT)  ||  5432,
+  
   username: process.env.PGUSER ||  'bonsaicorner',
   password: process.env.PGPASSWORD ||'admin123',
   database: process.env.PGDATABASE || 'bonsaicorner',
